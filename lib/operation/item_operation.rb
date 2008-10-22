f66096b65c714cfaab19c289a96c1ab555fae509
charlysisto <charly@charly-mac.local>
2008-10-22 22:39:11 +0200

    operation.rb abstracted : holds all essential instance methods

--- lib/operation/item_operation.rb
+++ lib/operation/item_operation.rb
@@ -28,35 +28,5 @@ module RAAWS
       end
     end
     
-    attr_accessor :operation, :search_index, :request, :response, :page
-    
-    def operation=(type)
-      name = self.class.to_s.split("::").last.gsub(/Operation/, ''). + type
-      @operation = { :operation => name }
-    end
-           
-    def search_index=(search_index)
-      @search_index = "RAAWS::#{search_index.to_s.camelize}".constantize.new
-    end
-    
-    def request=(params)
-      @request = returning Request.new do |req|
-        req.params = params.merge(operation)
-      end
-    end
-    
-    def response=(group)
-      @response = returning Response.new do |resp|
-        resp.group = group
-      end
-    end
-    
-    def result
-      raise "No request, no results" unless @request && @response
-      @response.hpricot = self.request.send.read
-      @result = @response.items
-    end
-
-    
   end  
 end
--- lib/operation/operation.rb
+++ lib/operation/operation.rb
@@ -8,12 +8,40 @@ module RAAWS
     VehiclePartLookUp VehiclePartSearch VehicleSearch>
 
   
-  class Operation    
-    def initialize
-    end 
+  class Operation
+    attr_accessor :operation, :search_index, :request, :response, :page
     
-    protected
+    def operation=(type)
+      name = self.class.to_s.split("::").last.gsub(/Operation/, '') + type
+      @operation = { :operation => name }
+    end
+           
+    def search_index=(search_index)
+      @search_index = "RAAWS::#{search_index.to_s.camelize}".constantize.new
+    end
+    
+    def request=(params)
+      @request = returning Request.new do |req|
+        req.params = params.merge(operation)
+      end
+    end
+    
+    def response=(group)
+      @response = returning Response.new do |resp|
+        resp.group = group
+      end
+    end
+    
+    def xml
+      raise "request needs to be initialized/set first" unless @request
+      @xml = self.request.send.read
+    end
+    
+    def result
+      raise "response needs to be initialized/set first" unless @response
+      @response.hpricot = self.xml
+      @result = @response.items
+    end
 
-    
   end
 end
