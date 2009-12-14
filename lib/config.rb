ad0e7a3186752586544fa973afb2c2a4d31bbd89
charlysisto <charly@charly-mac.local>
2009-12-14 11:29:21 +0100

    yaml config

--- lib/config.rb
+++ lib/config.rb
@@ -1,23 +1,10 @@
 module RAAWS
   class Config
     include Singleton
-    attr_accessor :lang, :data
+    attr_reader :data
     
-    FR = {
-      :domain => "webservices.amazon.fr",
-      :associate_tag => "charlieschapl-21",
-      :secret_key => 'secretekeyfromamazonwhydotheyneedthat'
-      }
-      
-    US = {
-      :domain => "webservices.amazon.com",
-      :associate_tag => "charliechap0e-20",
-      :secret_key => 'secretkeyfromamazonwhydotheyneedthat'      
-    }
-    
-    def lang=(lang)
-      @lang = lang
-      @data = @lang=="fr" ? FR : US
+    def data=(lang)
+      @data = YAML.load_file( File.dirname(__FILE__) + "/../config.yml" )[lang]
     end
   end
 end
