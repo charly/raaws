c9036d74788c35af941fc38684f26c24c633304e
charlysisto <charlysisto@gmail.com>
2012-07-17 11:25:17 +0200

    configures country

--- lib/config.rb
+++ lib/config.rb
@@ -1,9 +1,10 @@
 module RAAWS
   class Config
     include Singleton
-    attr_reader :data
+    attr_reader :data, :country
     
     def data=(lang)
+      @country = lang
       @data = YAML.load_file( File.dirname(__FILE__) + "/../config.yml" )[lang]
     end
   end
